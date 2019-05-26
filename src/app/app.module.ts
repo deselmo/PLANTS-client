a2a2dc4d8e978dede73b0c1059e226db5ec97675
William Guglielmo <william@deselmo.com>
2019-05-26 16:54:08 +0200

    Refactor api.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {MaterialModule} from '../material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material-module';
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PlantComponent } from './plant/plant.component';
